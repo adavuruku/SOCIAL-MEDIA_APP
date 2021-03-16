785e8cdd03732f8fdf9ede30b721dae5ce1c50b5
Abdulraheem Sherif Adavuruku <35297057+adavuruku@users.noreply.github.com>
2021-03-16 02:46:43 +0100

    started front end

--- front-end/src/components/auth/Register.js
+++ front-end/src/components/auth/Register.js
@@ -1,52 +1,34 @@
-import{React,Fragment, useState}from 'react';
+import React, { Fragment, useState } from 'react';
+import { connect } from 'react-redux';
 import { Link, Redirect } from 'react-router-dom';
-import axios from 'axios'
+import { setAlert } from '../../actions/alert';
+import PropTypes from 'prop-types';
 
-const Register =()=>{
-    //form state using react useState
-    const [formData, setFormData] = useState({
-        name:'',
-        email:'',
-        password:'',
-        password2:''
-    });
+const Register = ({ setAlert}) => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+  });
 
-    const { name, email, password, password2 } = formData;
+  const { name, email, password, password2 } = formData;
 
-    const onChange = (e) =>{
-        console.log(e.target.value)
-        setFormData({ ...formData, [e.target.name]: e.target.value });
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
 
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    if (password !== password2) {
+      setAlert('Passwords do not match', 'danger');
+    } else {
+      // register({ name, email, password });
+      setAlert('Succesfully Registered', 'success');
     }
-     
-      const onSubmit = async (e) => {
-        e.preventDefault();
-        if (password !== password2) {
-            console.log('Password Do Not Match')
-        } else {
-            console.log(formData)
-        //     const newUser = {
-        //         "fullName":name,
-        //         email,
-        //         password
-        //     }
-        //     try{
-        //         const config = {
-        //             headers:{
-        //                 'Content-Type':'application/json'
-        //             }
-        //         }
-        //         const body = JSON.stringify(newUser)
-        //         const res = await axios.post('/api/user/register',body,config)
-        //         console.log(res.data)
-        //     }catch(err){
-        //         console.log(err.response)
-        //     }
-        }
-      };
-    
-    return (
-        <Fragment>
+  };
+
+  return (
+    <Fragment>
       <h1 className="large text-primary">Sign Up</h1>
       <p className="lead">
         <i className="fas fa-user" /> Create Your Account
@@ -59,7 +41,6 @@ const Register =()=>{
             name="name"
             value={name}
             onChange={onChange}
-            required
           />
         </div>
         <div className="form-group">
@@ -82,7 +63,6 @@ const Register =()=>{
             name="password"
             value={password}
             onChange={onChange}
-            minLength="6"
           />
         </div>
         <div className="form-group">
@@ -92,7 +72,6 @@ const Register =()=>{
             name="password2"
             value={password2}
             onChange={onChange}
-            minLength="6"
           />
         </div>
         <input type="submit" className="btn btn-primary" value="Register" />
@@ -101,7 +80,13 @@ const Register =()=>{
         Already have an account? <Link to="/login">Sign In</Link>
       </p>
     </Fragment>
-    )
-}
+  );
+};
 
-export default Register
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired
+};
+
+
+
+export default connect(null, { setAlert })(Register);
